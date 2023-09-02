03b36e3153b1ddf2720c1b715c8be12438c774d6
bandao <1292008909@qq.com>
2023-09-02 10:32:29 +0800

    第一次PUSH

---
+++ src/main/java/com/ccsu/RegisterCenterApplication.java
@@ -0,0 +1,13 @@
+package com.ccsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RegisterCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterCenterApplication.class);
+    }
+}
---
+++ src/main/java/com/ccsu/controller/TransController.java
@@ -0,0 +1,12 @@
+package com.ccsu.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller("transController")
+public class TransController {
+
+    /**
+     *
+     */
+    public String
+}
