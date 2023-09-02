b04648fd18e993580b1dc54b3885af82eae279fe
Bandao-f <59996871+Bandao-f@users.noreply.github.com>
2023-09-02 10:40:24 +0800

    Add files via upload

---
+++ bandaotiehe/src/main/java/com/ccsu/controller/TransController.java
@@ -0,0 +1,14 @@
+package com.ccsu.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller("transController")
+public class TransController {
+
+    /**
+     *
+     */
+    public String submit(){
+        return "为啥不能PUSH";
+    }
+}
